033fe845c9b1c307d481c8d843640328e4bb9ac1
unknown <bvt@CSWINLAB10.ds.cecs.pdx.edu>
2012-10-02 11:37:16 -0700

    file reading finished, untested

--- Seat-Allocator/Program.cs
+++ Seat-Allocator/Program.cs
@@ -7,8 +7,34 @@ namespace Seat_Allocator
 {
     class Program
     {
+        static int[,] h;
+        static int total;
         static void Main(string[] args)
         {
+            System.IO.StreamReader file = new System.IO.StreamReader(args[1]);
+            total = Convert.ToInt32(file.ReadLine());
+
+            h = new int[total, total];
+            string[] s;
+            for (int i = 0; i < total; ++i)
+            {
+                s = file.ReadLine().Split(' ');
+                for (int j = 0; j < total; ++j)
+                {
+                    h[i, j] = Convert.ToInt32(s[j]);
+                }
+            }
+
+            Console.WriteLine(total);
+            for (int i = 0; i < total; ++i)
+            {
+                for (int j = 0; j < total; ++j)
+                {
+                    Console.Write(h[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
