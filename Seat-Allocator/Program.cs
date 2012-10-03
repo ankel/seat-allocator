64be53917d1106c147fcdc133bc6f3463f73e699
unknown <bvt@CSWINLAB10.ds.cecs.pdx.edu>
2012-10-03 15:39:12 -0700

    Read file tested

--- Seat-Allocator/Program.cs
+++ Seat-Allocator/Program.cs
@@ -7,33 +7,11 @@ namespace Seat_Allocator
 {
     class Program
     {
-        static int[,] h;
-        static int total;
         static void Main(string[] args)
         {
-            System.IO.StreamReader file = new System.IO.StreamReader(args[1]);
-            total = Convert.ToInt32(file.ReadLine());
-
-            h = new int[total, total];
-            string[] s;
-            for (int i = 0; i < total; ++i)
-            {
-                s = file.ReadLine().Split(' ');
-                for (int j = 0; j < total; ++j)
-                {
-                    h[i, j] = Convert.ToInt32(s[j]);
-                }
-            }
-
-            Console.WriteLine(total);
-            for (int i = 0; i < total; ++i)
-            {
-                for (int j = 0; j < total; ++j)
-                {
-                    Console.Write(h[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+            SeatInfo si = new SeatInfo();
+            si.ReadFile(args[1]);
+            si.Display();
             Console.ReadLine();
         }
     }
---
+++ Seat-Allocator/SeatInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seat_Allocator
+{
+    class SeatInfo
+    {
+        public int size;
+        public int[,] h;
+
+        public void Display()
+        {
+            Console.WriteLine(size);
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    Console.Write(h[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ReadFile(string path)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            size = Convert.ToInt32(file.ReadLine());
+
+            h = new int[size,size];
+
+            string[] s;
+            for (int i = 0; i < size; ++i)
+            {
+                s = file.ReadLine().Split(' ');
+                if (s.Length != size)
+                    throw new Exception("File malformat at line " + (i + 2));
+                for (int j = 0; j < size; ++j)
+                {
+                    h[i, j] = Convert.ToInt32(s[j]);
+                }
+            }
+        }
+    }
+}
