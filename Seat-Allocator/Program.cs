3de52e137f81d8fc0f66b33b6e9ecd27f9dde90e
unknown <bvt@CSWINLAB10.ds.cecs.pdx.edu>
2012-10-08 16:32:35 -0700

    working

--- Seat-Allocator/Program.cs
+++ Seat-Allocator/Program.cs
@@ -5,14 +5,25 @@ using System.Text;
 
 namespace Seat_Allocator
 {
+    /// <summary>
+    /// AI class PSU Fall 2012
+    /// Teacher: Bart Massey
+    /// Student: Binh Tran
+    /// 
+    /// Seat allocating problem. Solution implemented both complete search and local search (with random factor)
+    /// Unless specify otherwise, program will try the most suitable mode.
+    /// 
+    /// Copyleft, no right reserved. Feel free to do anything with it.
+    /// </summary>
     class Program
     {
 
         static int[] chart;
         static int score;
+        static bool debug = false;
 
         /// <summary>
-        /// Performce complete search based on info given by si to find the optimal configuration
+        /// Perform a complete search based on info given by si to find the optimal configuration
         /// </summary>
         /// <param name="si">Seat info given</param>
         static void CompleteSearch(SeatInfo si)
@@ -84,30 +95,131 @@ namespace Seat_Allocator
             }
         }
 
+        /// <summary>
+        /// Perform a local search with random move 50% of the time
+        /// </summary>
+        /// <param name="si">Seating info used to grade the seating scheme</param>
         static void StochasticSearch(SeatInfo si)
         {
             DateTime due = DateTime.Now.AddMinutes(1);
 
-            int[] temp;
-            int temptScore;
-
-            while (DateTime.Now <= due)
+            int[] temp = AssignRandomSeat(si.size);
+            int tempScore = si.Score(temp);
+            DateTime t = DateTime.Now;
+            int n = 0;      //how many time a move results in a suboptimal score / no good move is found
+            
+            while (t <= due)
             {
-                temp = AssignRandomSeat(si.size);
-                temptScore = si.Score(temp);
-                bool changed = false;
-
-                for (int i = 1; i <= si.size; ++i)
+                if (n == 0 || t.Millisecond % 2 == 0) // perform the "good" move if the last good-move attempt wasn't stuck
                 {
-                    for (int j = i + 1; j <= si.size; ++j)
-                    {
-                        int t = temp[i];
 
+                    GoodMove(si, ref temp, ref tempScore);
+                    if (tempScore > score)
+                    {
+                        Array.Copy(temp, chart, si.size + 1);
+                        score = tempScore;
+                        n = 0;
+                    }
+                    else
+                    {
+                        ++n;
                     }
                 }
+                else
+                {
+                    RandomMove(si, ref temp, ref tempScore);
+
+                    if (tempScore > score)
+                    {
+                        Array.Copy(temp, chart, si.size + 1);
+                        score = tempScore;
+                        n = 0;
+                    }
+                    else
+                    {
+                        ++n;
+                    }
+                }
+
+                if (n > 100) // stuck for the last 100 moves then restart
+                {
+                    temp = AssignRandomSeat(si.size);
+                    tempScore = si.Score(temp);
+                }
+
+                t = DateTime.Now;
             }
         }
 
+        /// <summary>
+        /// Perform a random move
+        /// </summary>
+        /// <param name="si">Seating info to grade the new scheme</param>
+        /// <param name="temp">current seating scheme</param>
+        /// <param name="tempScore">current score</param>
+        private static void RandomMove(SeatInfo si, ref int[] temp, ref int tempScore)
+        {
+            Random rand = new Random();
+
+            int i = rand.Next(si.size) + 1;
+            int j;
+            do
+            {
+                j = rand.Next(si.size) + 1;
+            } while (i == j);
+            Swap(ref temp, i, j);
+            tempScore = si.Score(temp);
+        }
+
+        /// <summary>
+        /// Perform a good move: look for the neighbour with the highest score
+        /// </summary>
+        /// <param name="si">seating info to grade the scheme</param>
+        /// <param name="temp">current seating scheme</param>
+        /// <param name="tempScore">current score</param>
+        private static void GoodMove(SeatInfo si, ref int[] temp, ref int tempScore)
+        {
+            int seatToSwap1 = 0, seatToSwap2 = 0;
+
+            for (int i = 1; i <= si.size; ++i)
+            {
+                for (int j = i + 1; j <= si.size; ++j)
+                {
+                    Swap(ref temp, i, j);
+                    if (si.Score(temp) > tempScore)
+                    {
+                        seatToSwap1 = i;
+                        seatToSwap2 = j;
+                        tempScore = si.Score(temp);
+                    }
+                    Swap(ref temp, i, j);
+                }
+            }
+            if (seatToSwap1 != 0 && seatToSwap2 != 0)
+            {
+                Swap(ref temp, seatToSwap1, seatToSwap2);
+            }
+            tempScore = si.Score(temp);
+        }
+
+        /// <summary>
+        /// Swap 2 elements of an array
+        /// </summary>
+        /// <param name="temp">the array</param>
+        /// <param name="i">1st position</param>
+        /// <param name="j">2nd position</param>
+        private static void Swap(ref int[] temp, int i, int j)
+        {
+            int t = temp[i];
+            temp[i] = temp[j];
+            temp[j] = t;
+        }
+
+        /// <summary>
+        /// Assign random people to random seat to generate a random seating scheme
+        /// </summary>
+        /// <param name="p">number to total people</param>
+        /// <returns>an array represent the random seating scheme</returns>
         private static int[] AssignRandomSeat(int p)
         {
             int[] ret = new int[p + 1];
@@ -131,27 +243,64 @@ namespace Seat_Allocator
             if (args.Length == 0)
             {
                 Console.WriteLine("Argument: ");
-                Console.WriteLine("g <num> <file>: generate a random seating info with <num> guests into <file>");
                 Console.WriteLine("c <file>: complete space state search using seating info from <file>");
                 Console.WriteLine("s <file>: stochastic search using seating info from <file>");
+                Console.WriteLine("a <file>: auto mode. Will try the most suitable search method");
                 return;
             }
 
             SeatInfo si = new SeatInfo();
-            //si.Gen(15);
-            //si.Display();
-            //si.ReadFile(@"..\..\TextFile1.txt");
-            //Console.Write("Complete searching...");
-            //CompleteSearch(si);
-            //Console.WriteLine("finished!");
-            //DisplaySeatingChart(chart, score);
-            //Console.WriteLine("Run time: " + (DateTime.Now - start));
+            si.ReadFile(args[1]);
+            chart = new int[si.size + 1];
+            score = 0;
 
-            int[] a = AssignRandomSeat(10);
-            for (int i = 1; i <= 10; ++i)
+            switch (args[0])
             {
-                Console.Write(a[i] + ", ");
+                case "c":
+                    Console.Write("Complete search...");
+                    CompleteSearch(si);
+                    Console.WriteLine(" finished!");
+                    break;
+                case "s":
+                    Console.Write("Local search... ");
+                    StochasticSearch(si);
+                    Console.WriteLine(" finished!");
+                    break;
+                case "a":
+                    if (si.size <= 11)
+                    {
+                        Console.Write("Complete search...");
+                        CompleteSearch(si);
+                        Console.WriteLine(" finished!");
+                    }
+                    else
+                    {
+                        Console.Write("Local search... ");
+                        StochasticSearch(si);
+                        Console.WriteLine(" finished!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Argument: ");
+                    Console.WriteLine("c <file>: complete space state search using seating info from <file>");
+                    Console.WriteLine("s <file>: stochastic search using seating info from <file>");
+                    Console.WriteLine("a <file>: auto mode. Will try the most suitable search method");
+                    return;
             }
+
+            if (debug)
+            {
+                si.Display();
+            }
+            Console.WriteLine(" finished!");
+            DisplaySeatingChart(chart, score);
+            Console.WriteLine("Run time: " + (DateTime.Now - start));
+
+            //int[] a = AssignRandomSeat(10);
+            //for (int i = 1; i <= 10; ++i)
+            //{
+            //    Console.Write(a[i] + ", ");
+            //}
 
             Console.ReadLine();
         }
